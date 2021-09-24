b655b7b20c57be903694e4dc77af77885b995c52
Babatunde Yakub <tundenasri@gmail.com>
2021-09-24 11:23:34 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server started running at port ${PORT}`));
